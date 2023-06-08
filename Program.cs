586ea4382f5f82cb7d0683ee53e48e316a686d8a
Mohammed Al Sakiti <mohammed110051@gmail.com>
2023-06-08 20:45:45 +0400

    Fibonacci number v1.1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter a number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int result = Fibonacci(n);
+
+            Console.WriteLine("The " + n + "th Fibonacci number is " + result);
         }
+
+        public static int Fibonacci(int n)
+        {
+            if (n <= 1)
+                return n;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        //Write a recursive function to calculate the factorial of a given number. The factorial of a non-negative integer n is the product of all positive integers less than or equal to n. 
+
+        //    Console.WriteLine("Enter a number:");
+        //    int n = Convert.ToInt32(Console.ReadLine());
+
+        //    //int res = Factorial(n);
+
+
+
+
+        //    Console.WriteLine("The factorial of " + n + " is " + res);
+        //}
     }
 }
